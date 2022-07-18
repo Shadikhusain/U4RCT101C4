d9a6375c61464f014bfac0cd04f5983d018f59f3
unknown <h52534396@gmail.com>
2022-07-18 16:50:57 +0530

    chalu

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <img src="" alt="" />
+    <p>this is a p tag</p>
+    </div>
+  );
+}
+
+export default App;
